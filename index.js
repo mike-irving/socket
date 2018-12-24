82e7f21059f62aeae43d50f1badb91d8c8482e00
mike-irving <46110649+mike-irving@users.noreply.github.com>
2018-12-25 01:30:10 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+﻿// Import packages
+const express = require("express");
+const socketIO = require("socket.io");
+const path = require("path");
+
+// Configuration
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+// Start server
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+ .listen(PORT, () => console.log("Listening on localhost:" + PORT));
+
+// Initiatlize SocketIO
+const io = socketIO(server);
+
+
+
+// Register "connection" events to the WebSocket
+io.on('connection', function(socket) {
+	var channel;
+	
+	socket.on('join', function (ch) {
+		channel = ch;
+		console.log('user connected => ' + channel);
+		
+		socket.join(channel);
+		
+		//io.emit('start', channel);
+		socket.emit('start', channel);
+		io.emit('hello', channel);
+		
+	});
+	
+	
+	
+	socket.on('score', function(value){
+		socket.to(channel).emit('score', value);
+		//io.emit('score', value);
+	});
+});
